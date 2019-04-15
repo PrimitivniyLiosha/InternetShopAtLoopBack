113c41a8c4c6ced0459def104e5a03a469d3d714
Leshen <lex.frol2020@gmail.com>
2019-04-15 18:29:07 +0300

    Added logic to User model

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,7 @@
-module.exports = (User) => {
-
+module.exports = (user) => {
+  user.afterRemote('create', async (ctx) => {
+    return await user.app.models.Cart.create({
+      userId: ctx.result.id
+    });
+  });
 };
