df6f4af99dd392b48dbc9a506b28829cf280f125
Leshen <lex.frol2020@gmail.com>
2019-04-17 15:03:18 +0300

    Added method which added to cart only products that are isAvailable

--- common/models/cart-item.js
+++ common/models/cart-item.js
@@ -1,3 +1,11 @@
 module.exports = (Cartitem) => {
-
+  Cartitem.observe('before save', async (ctx) => {
+    const product = await Cartitem.app.models.Product.findById(ctx.instance.productId);
+    if (!product.isAvailable) {
+      const error = new Error();
+      error.status = 412;
+      error.message = 'The product is out of stock';
+      throw error;
+    }
+  });
 };
