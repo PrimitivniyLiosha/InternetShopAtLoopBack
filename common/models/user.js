f4fa95baaf9a6a980ece9e3010868d3a3d890f3a
Leshen <lex.frol2020@gmail.com>
2019-04-17 14:57:53 +0300

    Added relation hasOne Cart in User.json and deleted return in User.js

--- common/models/user.js
+++ common/models/user.js
@@ -1,6 +1,6 @@
 module.exports = (user) => {
   user.afterRemote('create', async (ctx) => {
-    return await user.app.models.Cart.create({
+    await user.app.models.Cart.create({
       userId: ctx.result.id
     });
   });
