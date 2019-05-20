58fdeaffd164f02eeab2db7c1af0073d84356e0f
Leshen <lex.frol2020@gmail.com>
2019-05-20 11:57:22 +0300

    Added method which when changing the quantity changes the totalSum

--- common/models/cart-item.js
+++ common/models/cart-item.js
@@ -1,11 +1,43 @@
-module.exports = (Cartitem) => {
-  Cartitem.observe('before save', async (ctx) => {
-    const product = await app.models.Product.findById(ctx.instance.productId);
-    if (!product.isAvailable) {
-      const error = new Error();
-      error.status = 412;
-      error.message = 'The product is out of stock';
-      throw error;
+module.exports = (CartItem) => {
+  function Error (message, statusCode) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+
+  const preconditionFailed = 412;
+
+  CartItem.beforeRemote('create', async (ctx) => {
+    if (ctx.args.data && ctx.args.data.quantity >= 1) {
+      const product = await app.models.Product.findById(ctx.args.data.productId);
+      ctx.args.data.totalSum = product.price * ctx.args.data.quantity;
+
+      if (!product.isAvailable || ctx.args.data.quantity < 1) {
+        throw new Error('Product unavailable or indicated unacceptable quantity.', preconditionFailed);
+      }
+    }
+  });
+
+  CartItem.observe('before save', async (ctx) => {
+    if (ctx.currentInstance.quantity >= 1) {
+      const product = await app.models.Product.findById(ctx.currentInstance.productId);
+      ctx.currentInstance.totalSum = product.price * ctx.currentInstance.quantity;
+
+      if (!product.isAvailable) {
+        throw new Error('Product is out of stock.', preconditionFailed);
+      }
+    } else if (ctx.currentInstance.quantity < 1) {
+      throw new Error('Quantity should be more than zero.', preconditionFailed);
+    }
+
+    if (ctx.data && ctx.data.quantity >= 1) {
+      const product = await app.models.Product.findById(ctx.data.productId);
+      ctx.data.totalSum = product.price * ctx.data.quantity;
+
+      if (!product.isAvailable) {
+        throw new Error('Product is out of stock.', preconditionFailed);
+      }
+    } else if (ctx.data && ctx.data.quantity < 1) {
+      throw new Error('Quantity should be more than zero.', preconditionFailed);
     }
   });
 };
